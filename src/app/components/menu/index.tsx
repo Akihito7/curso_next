f1834c39e86903e314b84b842142cd3add5e10ce
akihito7 <akihitopro7@gmail.com>
2024-08-13 21:44:34 -0300

    app router,layout and metadata

---
+++ src/app/components/menu/index.tsx
@@ -0,0 +1,8 @@
+export function Menu(){
+  return (
+    <ul>
+      <li>Home</li>
+      <li>About</li>
+    </ul>
+  )
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,17 @@
-import type { Metadata } from "next";
 import "./globals.css";
 
+import type { Metadata } from "next";
+import { Menu } from "./components/menu";
+
 export const metadata: Metadata = {
-  title: "Dogs",
+  title: "Home",
   description: "Curso Next js Origamid",
 };
+
+/* 
+Pra definir o metadata da página você deve exportar uma const com exatamente o nome metadata
+que deve conter um objeto com as propriedades dos metadata que você quer definir.
+*/
 
 export default function RootLayout({
   children,
@@ -13,7 +20,25 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-BR">
-      <body>{children}</body>
+      <body>
+        <Menu />
+        {children}
+      </body>
     </html>
   );
 }
+
+/* 
+Layout basicamente é algo que vai ser compartilhado entre diferentes páginas,
+nesse caso usei o componente menu pra dar um exemplo e fixar a ideia.
+*/
+
+
+/* 
+O next não usa nenhuma lib pra cuidar das rotas como no react js,
+tudo o que deve ser feito é adicionar uma pasta na raiz da pasta app
+e dentro da pasta você deve ter um arquivo com o nome page.tsx ou page.js
+
+exemplo
+src/app/about/page.tsx
+*/
