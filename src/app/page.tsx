2e6a3554ce7c9cf237118c0ded9449c8ad6baf7c
akihito7 <akihitopro7@gmail.com>
2024-08-16 21:25:06 -0300

    a little bit about server actions

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
+'use client'
+import { setCookies } from "./actions/set-cookie";
+
 export default function Home() {
+  function handleSetCookies(){
+    setCookies("secret", "12345")
+  };
+
   return (
     <main>
       <h1>Hello NextJS</h1>
+      <button onClick={handleSetCookies}>Definir cookies</button>
     </main>
   );
 }
