d02841f2aa012d4982f1c8a47a627e8cbbf0fda8
akihito7 <akihitopro7@gmail.com>
2024-08-17 13:08:45 -0300

    clear project to exercise about cache

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,8 @@
-import { GetCookies } from "@/components/getCookies";
-import { getCookies } from "./server-actions/cookies";
-
 export default async function Home() {
-  const cookies = await getCookies("token"); 
+  
   return (
     <main>
       <h1>Hello NextJS</h1>
-      <GetCookies />
-      <p>{cookies?.value}</p>
     </main>
   );
 }
--- src/app/server-actions/revalidateTagsAction.tsx
+++
@@ -1,8 +0,0 @@
-'use server'
-
-import { revalidatePath, revalidateTag } from "next/cache"
-
-export async function RevalidateTagsAction() {
-  //revalidatePath("/actions")
-  revalidateTag("actions")
-}
